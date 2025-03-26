fda46212a411b2c1873cc2477f7aeed0af891ce8
Andrew Young <acydeveloper05@gmail.com>
2025-03-26 13:31:23 -0400

    Fixing errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'docs', dest: '' }  // copies 'docs' to 'dist/docs'
+      ]
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
 })
